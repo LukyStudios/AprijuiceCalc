21527b45dadc1172da50490780a8d06b574efd6b
LukyStudios <lukegifford4@gmail.com>
2025-12-29 21:53:13 -0600

    Added Berry Display Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { Button, Card, CardContent, Typography } from "@mui/material";
-
-import SelectWithDelete from "./components/SelectWithDelete.jsx";
-import EasySelect from "./components/EasySelect.jsx";
+import { Button, Card, CardContent, Typography, Stack } from "@mui/material";
 
 // #region Data
 import apricorns from "../public/data/apricorns.json";
 // #endregion
+
+import SelectWithDelete from "./components/SelectWithDelete.jsx";
+import EasySelect from "./components/EasySelect.jsx";
+import BerryDisplay from "./components/BerryDisplay.jsx";
 
 function App() {
   const [apricorn, setApricorn] = useState();
@@ -27,6 +28,42 @@ function App() {
             id="apricorn-selection"
             items={apricorns}
           />
+          <Stack
+            sx={{ width: "100%", display: "inline-flex", alignItems: "center" }}
+            direction="row"
+            spacing={3}
+          >
+            <BerryDisplay
+              selectedBerry={{
+                name: "Kee",
+                acceleration: 30,
+                skill: 30,
+                speed: 10,
+                stamina: 10,
+                jump: 10,
+              }}
+            />
+            <BerryDisplay
+              selectedBerry={{
+                name: "Kee",
+                acceleration: 30,
+                skill: 30,
+                speed: 10,
+                stamina: 10,
+                jump: 10,
+              }}
+            />
+            <BerryDisplay
+              selectedBerry={{
+                name: "Kee",
+                acceleration: 30,
+                skill: 30,
+                speed: 10,
+                stamina: 10,
+                jump: 10,
+              }}
+            />
+          </Stack>
         </CardContent>
       </Card>
     </>
---
+++ src/components/BerryDisplay.jsx
@@ -0,0 +1,49 @@
+import {
+  Card,
+  Typography,
+  Grid,
+  CardContent,
+  Divider,
+  Box,
+} from "@mui/material";
+
+export default function BerryDisplay({ selectedBerry, ...props }) {
+  console.log(selectedBerry);
+
+  // TODO: Add Image of Berry
+  return (
+    <Card>
+      <CardContent>
+        <Typography variant="h2">{selectedBerry.name} Berry</Typography>
+        <Grid container spacing={1}>
+          {GridRow("Acceleration", selectedBerry.acceleration)}
+          {GridRow("Skill", selectedBerry.skill)}
+          {GridRow("Speed", selectedBerry.speed)}
+          {GridRow("Stamina", selectedBerry.stamina)}
+          {GridRow("Jump", selectedBerry.jump)}
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+}
+
+function GridRow(left, right) {
+  return (
+    <Box
+      sx={{
+        display: "inline-flex",
+        alignItems: "center",
+        border: "1px solid",
+        borderColor: "divider",
+        borderRadius: 2,
+        bgcolor: "background.paper",
+        color: "text.secondary",
+        p:1
+      }}
+    >
+      <Typography>{left}</Typography>
+      <Divider orientation="vertical" sx={{m:1}}/>
+      <Typography>{right}</Typography>
+    </Box>
+  );
+}
