a107b87a79845da0bfe2a32fa3a6c6bf35af9955
LukyStudios <lukegifford4@gmail.com>
2025-12-31 14:28:47 -0600

    Update FlavorBar.jsx

--- src/components/displays/rows/FlavorBar.jsx
+++ src/components/displays/rows/FlavorBar.jsx
@@ -1,6 +1,8 @@
 import { LinearProgress, Typography, Grid } from "@mui/material"
 
 export default function FlavorBar(stat, flavor, value) {
+    // TODO: Add tick marks for thresholds
+
     return <Grid
         container
         direction="row"
@@ -15,7 +17,7 @@ export default function FlavorBar(stat, flavor, value) {
             <Typography>{stat}</Typography>
         </Grid>
         <Grid size={5}>
-            <LinearProgress variant="determinate" value={flavor / 105 * 100} />
+            <LinearProgress variant="determinate" value={flavor / 105 * 100} /> 
         </Grid>
         <Grid size={2}>
             <Typography align='right'>{value}</Typography>
