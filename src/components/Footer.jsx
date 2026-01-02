df274544430f2367a07a8d84a8c9bd3bfb8ac664
LukyStudios <lukegifford4@gmail.com>
2026-01-01 23:48:16 -0600

    Footer that doesnt cover stats! (oops)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,35 +2,34 @@ import { blueGrey } from "@mui/material/colors";
 import { Box, Typography } from "@mui/material";
 
 export default function Footer() {
-    return <Box
-        sx={{
+    return <footer>
+        <Box sx={{
             p: 1,
-            position: 'fixed',
             left: 0,
             bottom: 0,
             width: '100%',
             color: blueGrey[900],
-            bgcolor: blueGrey[200],
             textAlign: "center",
             justifyContent: "center",
             alignItems: "center",
-        }}
-    >
-        <Typography variant="caption" sx={{
-            justifyContent: "center",
-            alignItems: "center",
-            verticalAlign: "middle"
-        }}>If you find this tool helpful, please consider to <a href="https://ko-fi.com/lukystudios/">
-                <img
-                    style={{ verticalAlign: "middle" }}
-                    src={"./images/support_me_on_kofi_blue.png"}
-                    alt={"Support me on KoFi"}
-                    width="98"
-                    height="20"
-                />
-            </a> !
-            <br /> Please report any bugs or leave feedback by <a href="https://github.com/lukystudios/aprijuicecalc/issues/new">opening an issue on GitHub</a>
-            <br /> All images & data are copied from Cobblemon's <a href="https://wiki.cobblemon.com/index.php/Aprijuice">Aprijuice</a> & <a href="https://wiki.cobblemon.com/index.php/Seasoning">Seasoning</a> Wikis as of v1.7.1
-        </Typography>
-    </ Box >
+        }}>
+            <br />
+            <Typography variant="caption" sx={{
+                justifyContent: "center",
+                alignItems: "center",
+                verticalAlign: "middle"
+            }}>If you find this tool helpful, please consider to <a href="https://ko-fi.com/lukystudios/">
+                    <img
+                        style={{ verticalAlign: "middle", imageRendering:'smooth' }}
+                        src={"./images/support_me_on_kofi_blue.png"}
+                        alt={"Support me on KoFi"}
+                        width="150"
+                        height="30"
+                    />
+                </a> !
+                <br /> Please report any bugs or leave feedback by <a href="https://github.com/lukystudios/aprijuicecalc/issues/new">opening an issue on GitHub</a>
+                <br /> All images & data are copied from Cobblemon's <a href="https://wiki.cobblemon.com/index.php/Aprijuice">Aprijuice</a> & <a href="https://wiki.cobblemon.com/index.php/Seasoning">Seasoning</a> Wikis as of v1.7.1
+            </Typography>
+        </Box>
+    </ footer>
 }
