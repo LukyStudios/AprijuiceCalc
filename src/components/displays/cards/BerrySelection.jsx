695b6dc40f9705d9ecce4dbc861a214e40df8940
LukyStudios <lukegifford4@gmail.com>
2026-02-08 12:32:37 -0600

    Grid Selection actually works now

--- src/components/displays/cards/BerrySelection.jsx
+++ src/components/displays/cards/BerrySelection.jsx
@@ -19,7 +19,8 @@ export default function BerrySelection({
 
   return (
     berry && selected && (
-      isGrid && <Card>
+      isGrid &&
+      (<Card>
         <CardContent align="center">
           <img
             src={"./images/berries/" + berry.name + "_Berry.png"}
@@ -61,55 +62,56 @@ export default function BerrySelection({
           </Stack>
         </CardContent>
       </Card>
-    ) || (
-      <Card>
-        <CardContent align="center">
-          <Stack direction="row" container spacing={1} sx={{
-            justifyContent: "space-evenly",
-            alignItems: "center",
-          }}>
-            <div>
-              <img
-                src={"./images/berries/" + berry.name + "_Berry.png"}
-                alt={berry.name + " Berry"}
-                width="48"
-                height="48"
-              />
-              <NumberSpinner
-                value={amount}
-                setValue={setAmount}
-                addBerry={addBerry}
-                removeBerry={removeBerry}
-                berry={berry}
-                min="0"
-                max="3"
-              />
-            </div>
-            <div>
-              {viewFlavor && (
-                <Stack container spacing={1} sx={{ justifyContent: 'space-between' }}>
-                  <Typography align="left" variant="h5">
-                    {berry.name} Berry
-                  </Typography>
-                  <Stack direction="row" container spacing={1}>
-                    <Row flavor="Spicy" >
-                      <Typography sx={{ minWidth: '18px' }}>{berry.spicy}</Typography>
-                    </Row>
-                    <Row flavor="Dry"><Typography sx={{ minWidth: '18px' }}>{berry.dry}</Typography></Row>
-                    <Row flavor="Sweet">
-                      <Typography sx={{ minWidth: '18px' }}>{berry.sweet}</Typography>
-                    </Row>
-                    <Row flavor="Sour"><Typography sx={{ minWidth: '18px' }}>{berry.sour}</Typography></Row>
-                    <Row flavor="Bitter">
-                      <Typography sx={{ minWidth: '18px' }}>{berry.bitter}</Typography>
-                    </Row>
-                  </Stack>
-                </ Stack>
-              )}
-            </div>
-          </Stack>
-        </CardContent>
-      </Card>
+      ) || (
+        <Card>
+          <CardContent align="center">
+            <Stack direction="row" container spacing={1} sx={{
+              justifyContent: "space-evenly",
+              alignItems: "center",
+            }}>
+              <div>
+                <img
+                  src={"./images/berries/" + berry.name + "_Berry.png"}
+                  alt={berry.name + " Berry"}
+                  width="48"
+                  height="48"
+                />
+                <NumberSpinner
+                  value={amount}
+                  setValue={setAmount}
+                  addBerry={addBerry}
+                  removeBerry={removeBerry}
+                  berry={berry}
+                  min="0"
+                  max="3"
+                />
+              </div>
+              <div>
+                {viewFlavor && (
+                  <Stack container spacing={1} sx={{ justifyContent: 'space-between' }}>
+                    <Typography align="left" variant="h5">
+                      {berry.name} Berry
+                    </Typography>
+                    <Stack direction="row" container spacing={1}>
+                      <Row flavor="Spicy" >
+                        <Typography sx={{ minWidth: '18px' }}>{berry.spicy}</Typography>
+                      </Row>
+                      <Row flavor="Dry"><Typography sx={{ minWidth: '18px' }}>{berry.dry}</Typography></Row>
+                      <Row flavor="Sweet">
+                        <Typography sx={{ minWidth: '18px' }}>{berry.sweet}</Typography>
+                      </Row>
+                      <Row flavor="Sour"><Typography sx={{ minWidth: '18px' }}>{berry.sour}</Typography></Row>
+                      <Row flavor="Bitter">
+                        <Typography sx={{ minWidth: '18px' }}>{berry.bitter}</Typography>
+                      </Row>
+                    </Stack>
+                  </ Stack>
+                )}
+              </div>
+            </Stack>
+          </CardContent>
+        </Card>
+      )
     )
   );
 }
